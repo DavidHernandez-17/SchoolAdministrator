c11671ded4ba6bc9459db3cfdd968dc9553b8618
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-03-28 18:57:21 -0500

    Finish the CRUD institutions.

---
+++ SchoolAdministrator/Controllers/InstitutionsController.cs
@@ -0,0 +1,154 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolAdministrator.Data;
+using SchoolAdministrator.Data.Entities;
+
+namespace SchoolAdministrator.Controllers
+{
+    public class InstitutionsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public InstitutionsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Institutions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Institutions.ToListAsync());
+        }
+
+        // GET: Institutions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var institution = await _context.Institutions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            return View(institution);
+        }
+
+        // GET: Institutions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Institutions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,InstitutionType")] Institution institution)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(institution);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(institution);
+        }
+
+        // GET: Institutions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var institution = await _context.Institutions.FindAsync(id);
+            if (institution == null)
+            {
+                return NotFound();
+            }
+            return View(institution);
+        }
+
+        // POST: Institutions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,InstitutionType")] Institution institution)
+        {
+            if (id != institution.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(institution);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstitutionExists(institution.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(institution);
+        }
+
+        // GET: Institutions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var institution = await _context.Institutions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            return View(institution);
+        }
+
+        // POST: Institutions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var institution = await _context.Institutions.FindAsync(id);
+            _context.Institutions.Remove(institution);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstitutionExists(int id)
+        {
+            return _context.Institutions.Any(e => e.Id == id);
+        }
+    }
+}
