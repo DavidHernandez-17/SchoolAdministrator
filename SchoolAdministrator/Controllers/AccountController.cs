87f86cd081d623ba687c8a49daabb5008c007bde
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-04-12 22:22:36 -0500

    Finish Redirect

--- SchoolAdministrator/Controllers/AccountController.cs
+++ SchoolAdministrator/Controllers/AccountController.cs
@@ -45,6 +45,12 @@ namespace SchoolAdministrator.Controllers
             await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult NotAuthorized()
+        {
+            return View();
+        }
+
     }
 
 }
--- SchoolAdministrator/Program.cs
+++ SchoolAdministrator/Program.cs
@@ -25,7 +25,11 @@ builder.Services.AddIdentity<User, IdentityRole>(cfg =>
     cfg.Password.RequiredLength = 6;
 }).AddEntityFrameworkStores<DataContext>();
 
-
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/NotAuthorized";
+    options.AccessDeniedPath = "/Account/NotAuthorized";
+});
 
 
 builder.Services.AddControllersWithViews();
@@ -54,6 +58,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/error/{0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
