29488e7374ed3581bfde0c1164fdf673a8201aa2
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-05-10 21:23:21 -0500

    Add ProductImageModel and AASubjectProductViewModel, Edit, Create, Datails.

--- SchoolAdministrator/Controllers/ProductsController.cs
+++ SchoolAdministrator/Controllers/ProductsController.cs
@@ -105,5 +105,280 @@ namespace SchoolAdministrator.Controllers
         }
 
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            EditProductViewModel model = new()
+            {
+                Description = product.Description,
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreateProductViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                Product product = await _context.Products.FindAsync(model.Id);
+                product.Description = model.Description;
+                product.Name = model.Name;
+                product.Price = model.Price;
+                product.Stock = model.Stock;
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                ModelState.AddModelError(string.Empty, exception.Message);
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Subject)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> AddImage(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            AddProductImageViewModel model = new()
+            {
+                ProductId = product.Id,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddImage(AddProductImageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "products");
+                }
+
+                Product product = await _context.Products.FindAsync(model.ProductId);
+                ProductImage productImage = new()
+                {
+                    Product = product,
+                    ImageId = imageId,
+                };
+
+                try
+                {
+                    _context.Add(productImage);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = product.Id });
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteImage(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductImage productImage = await _context.ProductImages
+                .Include(pi => pi.Product)
+                .FirstOrDefaultAsync(pi => pi.Id == id);
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+
+            await _blobHelper.DeleteBlobAsync(productImage.ImageId, "products");
+            _context.ProductImages.Remove(productImage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Details), new { Id = productImage.Product.Id });
+        }
+
+
+        public async Task<IActionResult> AddSubject(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            AddSubjectProductViewModel model = new()
+            {
+                ProductId = product.Id,
+                Subjects = await _combosHelper.GetComboSubjectsAsync(),
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSubject(AddSubjectProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = await _context.Products.FindAsync(model.ProductId);
+                ProductCategory productCategory = new()
+                {
+                    Subject = await _context.Subjects.FindAsync(model.SubjectId),
+                    Product = product,
+                };
+
+                try
+                {
+                    _context.Add(productCategory);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = product.Id });
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductCategory productCategory = await _context.ProductCategories
+                .Include(pc => pc.Product)
+                .FirstOrDefaultAsync(pc => pc.Id == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductCategories.Remove(productCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Details), new { Id = productCategory.Product.Id });
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products
+                .Include(p => p.ProductCategories)
+                .Include(p => p.ProductImages)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Product product = await _context.Products
+                .Include(p => p.ProductImages)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            foreach (ProductImage productImage in product.ProductImages)
+            {
+                await _blobHelper.DeleteBlobAsync(productImage.ImageId, "products");
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
     }
 }
---
+++ SchoolAdministrator/Models/AddSubjectProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAdministrator.Models
+{
+    public class AddSubjectProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Display(Name = "Materia")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar una categoría.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int SubjectId { get; set; }
+
+        public IEnumerable<SelectListItem> Subjects { get; set; }
+    }
+
+}
