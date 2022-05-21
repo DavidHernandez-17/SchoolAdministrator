1f8b916991db9af5e2e7efc2f1216b7738e705b8
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-05-21 14:28:50 -0500

    Add Spinner and busyIndicator, DeleteDialog

---
+++ SchoolAdministrator/Migrations/20220520025702_AddInscriptionInUserModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministrator.Migrations
+{
+    public partial class AddInscriptionInUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Inscription",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inscription",
+                table: "AspNetUsers");
+        }
+    }
+}
