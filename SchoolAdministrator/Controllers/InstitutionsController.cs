dda3a4a3a47d1f04607d243f0eb1ce708f33ae85
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-05-22 18:46:19 -0500

    ModelInstitution version one

--- SchoolAdministrator/Controllers/InstitutionsController.cs
+++ SchoolAdministrator/Controllers/InstitutionsController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SchoolAdministrator.Data;
 using SchoolAdministrator.Data.Entities;
+using SchoolAdministrator.Helpers;
 using SchoolAdministrator.Models;
 using Vereyon.Web;
+using static SchoolAdministrator.Helpers.ModalHelper;
 
 namespace SchoolAdministrator.Controllers
 {
@@ -47,6 +49,7 @@ namespace SchoolAdministrator.Controllers
 
             return View(institution);
         }
+
 
         public async Task<IActionResult> DetailsLevel(int? id)
         {
@@ -102,8 +105,8 @@ namespace SchoolAdministrator.Controllers
                 }
             }
             return View(institution);
-        
-    }
+
+        }
 
         public async Task<IActionResult> AddLevel(int? id)
         {
@@ -142,7 +145,7 @@ namespace SchoolAdministrator.Controllers
                     };
                     _context.Add(level);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Details), new {Id = model.Id });
+                    return RedirectToAction(nameof(Details), new { Id = model.Id });
                 }
                 catch (DbUpdateException dbUpdateException)
                 {
@@ -182,9 +185,6 @@ namespace SchoolAdministrator.Controllers
             return View(institution);
         }
 
-        // POST: Institutions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Institution institution)
@@ -220,7 +220,7 @@ namespace SchoolAdministrator.Controllers
             }
 
             return View(institution);
-        
+
         }
 
         public async Task<IActionResult> EditLevel(int? id)
@@ -231,7 +231,7 @@ namespace SchoolAdministrator.Controllers
             }
 
             Level level = await _context.Levels
-                .Include(l =>l.Institution)
+                .Include(l => l.Institution)
                 .FirstOrDefaultAsync(l => l.Id == id);
             if (level == null)
             {
@@ -248,9 +248,7 @@ namespace SchoolAdministrator.Controllers
             return View(model);
         }
 
-        // POST: Institutions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditLevel(int id, LevelViewModel model)
@@ -272,7 +270,7 @@ namespace SchoolAdministrator.Controllers
                     };
                     _context.Update(level);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Details), new {Id = model.InstitutionId });
+                    return RedirectToAction(nameof(Details), new { Id = model.InstitutionId });
                 }
                 catch (DbUpdateException dbUpdateException)
                 {
@@ -295,35 +293,6 @@ namespace SchoolAdministrator.Controllers
 
         }
 
-        // GET: Institutions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var institution = await _context.Institutions
-                .Include(m => m.Levels)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (institution == null)
-            {
-                return NotFound();
-            }
-
-            return View(institution);
-        }
-
-        // POST: Institutions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var institution = await _context.Institutions.FindAsync(id);
-            _context.Institutions.Remove(institution);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
         private bool InstitutionExists(int id)
         {
@@ -348,7 +317,7 @@ namespace SchoolAdministrator.Controllers
             return View(level);
         }
 
-        
+
         [HttpPost, ActionName("DeleteLevel")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteLevelConfirmed(int id)
@@ -358,12 +327,111 @@ namespace SchoolAdministrator.Controllers
                 .FirstOrDefaultAsync(l => l.Id == id);
             _context.Levels.Remove(level);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Details), new {Id = level.Institution.Id});
+            return RedirectToAction(nameof(Details), new { Id = level.Institution.Id });
+        }
+
+
+        [NoDirectAccess]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Institution institution = await _context.Institutions.FirstOrDefaultAsync(c => c.Id == id);
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Institutions.Remove(institution);
+                await _context.SaveChangesAsync();
+                _flashMessage.Info("Registro borrado.");
+            }
+            catch
+            {
+                _flashMessage.Danger("No se puede borrar la institución porque tiene registros relacionados.");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [NoDirectAccess]
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Institution());
+            }
+            else
+            {
+                Institution institution = await _context.Institutions.FindAsync(id);
+                if (institution == null)
+                {
+                    return NotFound();
+                }
+
+                return View(institution);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, Institution institution)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (id == 0) //Insert
+                    {
+                        _context.Add(institution);
+                        await _context.SaveChangesAsync();
+                        _flashMessage.Info("Registro creado.");
+                    }
+                    else //Update
+                    {
+                        _context.Update(institution);
+                        await _context.SaveChangesAsync();
+                        _flashMessage.Info("Registro actualizado.");
+                    }
+                    return Json(new
+                    {
+                        isValid = true,
+                        html = ModalHelper.RenderRazorViewToString(
+                            this,
+                            "_ViewAll",
+                            _context.Institutions
+                                .Include(c => c.Levels)
+                                .ToList())
+                    });
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        _flashMessage.Danger("Ya existe una institución con el mismo nombre.");
+                    }
+                    else
+                    {
+                        _flashMessage.Danger(dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    _flashMessage.Danger(exception.Message);
+                }
+            }
+
+            return Json(new { isValid = false, html = ModalHelper.RenderRazorViewToString(this, "AddOrEdit", institution) });
         }
 
 
     }
 
-    
-        
+
+
 }
